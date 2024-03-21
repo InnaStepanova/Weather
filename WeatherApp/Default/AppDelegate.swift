853f1492c913b3f2cb0d597e96f9a7c601e58424
InnaStepanova <inna-panichkina@yandex.ru>
2024-03-21 14:51:15 +0300

    Add .gitignore

--- WeatherApp/Default/AppDelegate.swift
+++ WeatherApp/Default/AppDelegate.swift
@@ -9,28 +9,36 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    
+    var window: UIWindow?
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        createAndShowStartVC()
+        
         return true
     }
+}
 
-    // MARK: UISceneSession Lifecycle
+// MARK: - Initial application settings
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+private extension AppDelegate {
+    /// Создание и отображение стартового ViewController
+    func createAndShowStartVC() {
+        let mainVC = ViewController()
+        let navigationController = UINavigationController(rootViewController: mainVC)
+        
+//        PresentationAssembly().weatherScreen.config(
+//            view: mainVC,
+//            navigationController: navigationController
+//        )
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
